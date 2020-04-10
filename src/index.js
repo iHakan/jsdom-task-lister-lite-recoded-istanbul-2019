f80f6195033f3d208a5ca47ecbd4b00a4f6f87c5
Hakan Yildiz <hakan@re-coded.com>
2020-04-10 14:00:31 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code 
+  let ul = document.getElementById("tasks");
   document.addEventListener("submit", function(e){
     e.preventDefault();
     
-    const creatingList = document.createElement("li");
+    //li yi olustur
+    let node = document.createElement("li");
     
-    const newTaskDescription = document.getElementById("new-task-description").value;
+    //Input a girilen değeri declare et 
+    let messageText = document.getElementById("new-task-description").value;
     
-    creatingList.innerHTML = `${newTaskDescription}`;
+    //Her listenin yanına kapatma butonu oluştur
+    let closeButton = document.createElement("button");
+    closeButton.innerHTML = "X";
     
-    document.getElementById("tasks").appendChild(creatingList);
+    //message ı li nin içine koy
+    node.innerHTML = `${messageText}`;
+    node.setAttribute("class","ta");
     
+    //butonu listenin içine koy
+    node.appendChild(closeButton);
+
     
-  });
+    //li i ul nin içine koy
+    document.getElementById("tasks").appendChild(node);
+     
+   });
+   
+   ul.addEventListener("click", function(e){
+     if(e.target && e.target.matches("node.ta")){
+       e.target.id = "del";
+       let deleteItem = document.getElementById("del");
+       ul.removeChild(deleteItem);  
+     }
+   })
    
 });
